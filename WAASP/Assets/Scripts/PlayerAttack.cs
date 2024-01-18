aa5b2a67b97497e991736a531dc36ae204ce4392
lilsusboi89 <lilsusboi89@gmail.com>
2024-01-18 13:52:53 -0800

    Added Functional shoot joystick, updated art assets, placeholder UI text added

--- WAASP/Assets/Scripts/PlayerAttack.cs
+++ WAASP/Assets/Scripts/PlayerAttack.cs
@@ -9,13 +9,19 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] AudioClip attackSound;
     [SerializeField] float bulletSpeed = 10f;
     [SerializeField] bool mouseShoot = true;
+    [SerializeField] bool joystickShoot = false;
+    public FixedJoystick joystick1;
     [SerializeField] float timeToFire = 0.7f;
     [SerializeField] float DestructionTimer = 0.29f;
+    [SerializeField] TMPro.TextMeshProUGUI ammoText;
+    private int ammoAmmount;
     float timer = 0;
     float x = 1;
     float y = 0;
     void Start()
     {
+        ammoAmmount = 10;
+
 
     }
 
@@ -24,6 +30,8 @@ public class PlayerAttack : MonoBehaviour
         
 
         timer += Time.deltaTime;
+        ammoText.SetText(ammoAmmount);
+
 
         if (mouseShoot)
         {
@@ -35,19 +43,24 @@ public class PlayerAttack : MonoBehaviour
             x = shootDirection.x;
             y = shootDirection.y;
         }
-
         else
         {
+            Vector3 shootDirection = joystick1.Direction;
+            shootDirection.z = 0;
+            shootDirection.Normalize();
+            x = shootDirection.x;
+            y = shootDirection.y;
+            /*
             float tempX = Input.GetAxisRaw("Horizontal");
             float tempY = Input.GetAxisRaw("Vertical");
             if (tempX != 0 || tempY != 0)
             {
                 x = tempX;
                 y = tempY;
-            }
+            }*/
         }
 
-        if (Input.GetButton("Fire1") && timer >= timeToFire)
+        if (joystick1.Direction.magnitude > 0 && timer >= timeToFire && ammoAmmount > 0)
         {
             if(Time.deltaTime == 0)
             {
@@ -60,6 +73,7 @@ public class PlayerAttack : MonoBehaviour
                 Bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * bulletSpeed;
                 Destroy(Bullet, DestructionTimer);
                 Camera.main.GetComponent<AudioSource>().PlayOneShot(attackSound);
+                ammoAmmount -= 1;
                 Debug.Log("Pew!");
                 timer = 0;
             }
